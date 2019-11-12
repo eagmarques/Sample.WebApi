cfe5907a7af7021233097f67180c24890699b47d
eagmarquesdev-principal <eagmarquesdev@gmail.com>
2019-11-12 15:40:19 -0300

    ...

---
+++ src/Sample.WebApiDemo/Sample.WebApiDemo/Controllers/WebController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Sample.WebApiDemo.Controllers
+{
+    public class WebController : ApiController
+    {
+        public static List<string> Items = new List<string> { "item1", "item2", "item3", "item4" };
+
+        [HttpGet]
+        public IEnumerable<string> GetItems()
+        {
+            return Items;
+        }
+
+        [HttpGet]
+        public string GetItemByIndex(int id)
+        {
+            return Items[id];
+        }
+    }
+}
